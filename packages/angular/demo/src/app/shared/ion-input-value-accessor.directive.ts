091bbe4ddf3dd496d8c769027ee482ceccf02835
Ken Sodemann <ken@ionic.io>
2017-11-15 14:03:22 -0600

    WIP - get it working with custom value accessor

--- packages/angular/demo/src/app/shared/ion-input-value-accessor.directive.ts
+++ packages/angular/demo/src/app/shared/ion-input-value-accessor.directive.ts
@@ -1,9 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { ControlValueAccessor, DefaultValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
   selector: 'ion-input',
   providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: IonInputValueAccessorDirective, multi: true }]
 })
-export class IonInputValueAccessorDirective extends DefaultValueAccessor implements ControlValueAccessor  {
+export class IonInputValueAccessorDirective implements ControlValueAccessor  {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    console.log('my value accessor');
+    this.onChange = () => {};
+  }
+
+  onChange: (value: string) => void;
+
+  writeValue(value: string) {
+    this.renderer.setProperty(this.element.nativeElement, 'value', value);
+  }
+
+  @HostListener('input', ['$event.target.value'])
+  _handleIllyChange(value: string) {
+    this.onChange(value);
+  }
+
+  registerOnChange(fn: (value: string) => void) {
+    this.onChange = value => {
+      fn(value);
+    };
+  }
+
+  registerOnTouched() {}
 }
